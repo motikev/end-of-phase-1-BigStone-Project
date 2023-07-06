fda4b0fd39a7a68a29d1d05eca2eaafb61746c32
motikev <kmotties@gmail.com>
2023-07-07 01:11:39 +0300

    changes on db.json server.js index.js

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
 const express = require('express');
 const app = express();
+
+// Define routes and endpoints here
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
